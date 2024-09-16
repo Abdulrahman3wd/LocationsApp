1cfa532cfafdba6c475c20ad840efa8338acc2e8
AbdulrahmanAwad <Abdulrahmanawad7861991@gmail.com>
2024-09-16 17:18:27 +0300

    Add project files.

---
+++ LocationsApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LocationsApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LocationsApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LocationsApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
